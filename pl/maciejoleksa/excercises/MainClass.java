ea0b5b0c83fae76cea78aa4bd16114ab77821454
Ketus-home <maciej.oleksa@gmail.com>
2015-07-24 19:37:44 +0200

    removed merged conflicts

--- pl/maciejoleksa/excercises/MainClass.java
+++ pl/maciejoleksa/excercises/MainClass.java
@@ -1,16 +1,14 @@
 package pl.maciejoleksa.excercises;
 
-<<<<<<< HEAD
+
 import java.io.IOException;
 import java.util.ArrayList;
-=======
->>>>>>> 9cbc0cf6728627ad3cafda698366afd0fa92b730
+
 
 public class MainClass {
 
 	public static void main(String[] args) throws ClassNotFoundException, IOException {
-		
-<<<<<<< HEAD
+
 			FileOperation nowy = new FileOperation();
 			ArrayList<String> tempList = new ArrayList<>();
 			
@@ -32,8 +30,6 @@ public class MainClass {
 				long endTime   = System.currentTimeMillis();
 				long totalTime = endTime - startTime;
 				System.out.println(totalTime);
-=======
-			
->>>>>>> 9cbc0cf6728627ad3cafda698366afd0fa92b730
+
 	}
 }
